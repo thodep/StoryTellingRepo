b3994b11c2d3863144afc7a28c768c48daa73608
thodep <thodep@yahoo.com>
2015-08-10 16:59:02 -0400

    Create a variable

--- GroupStorytellingPlayground.playground/Contents.swift
+++ GroupStorytellingPlayground.playground/Contents.swift
@@ -3,3 +3,5 @@
 import UIKit
 
 var firstStorySentence = "In the ancient land of Eye Ohess there was a problem."
+
+var thoLine = "Lets help each other in the next 9 weeks and build some cool apps "
