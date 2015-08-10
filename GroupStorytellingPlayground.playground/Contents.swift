be466fd292de61b74d49b36272968495f67fd53c
Miwand <miwand.najafe@gmail.com>
2015-08-10 17:29:41 -0400

    fourth sentence

--- GroupStorytellingPlayground.playground/Contents.swift
+++ GroupStorytellingPlayground.playground/Contents.swift
@@ -7,3 +7,5 @@ var firstStorySentence = "In the ancient land of Eye Ohess there was a problem."
 var thoLine = "Lets help each other in the next 9 weeks and build some cool apps "
 
 var nextLine = "Indeed, we shall work together to meet our goals."
+
+var mnightshyamaLine = "Our goals will be met only through the destruction of Eye Ohess"
