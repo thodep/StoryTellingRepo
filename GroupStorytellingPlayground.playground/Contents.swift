a906caae2d9dc285653f670d0846e586bf761a57
Shameem Rahman <srahman@Shameems-MacBook-Pro.local>
2015-08-10 17:35:34 -0400

    Added fourth line.

--- GroupStorytellingPlayground.playground/Contents.swift
+++ GroupStorytellingPlayground.playground/Contents.swift
@@ -9,3 +9,5 @@ var thoLine = "Lets help each other in the next 9 weeks and build some cool apps
 var nextLine = "Indeed, we shall work together to meet our goals."
 
 var mnightshyamaLine = "Our goals will be met only through the destruction of Eye Ohess"
+
+var fourthLine = "The goal is that we do not have a merge conflict."
