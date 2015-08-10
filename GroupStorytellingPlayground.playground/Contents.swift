a1700c266b8ad24e5b3c45699577f877fd13d377
Alexander Besson <Alex@Alexanders-MacBook-Pro.local>
2015-08-10 17:25:16 -0400

    Alex's edit

--- GroupStorytellingPlayground.playground/Contents.swift
+++ GroupStorytellingPlayground.playground/Contents.swift
@@ -5,3 +5,5 @@ import UIKit
 var firstStorySentence = "In the ancient land of Eye Ohess there was a problem."
 
 var thoLine = "Lets help each other in the next 9 weeks and build some cool apps "
+
+var nextLine = "Indeed, we shall work together to meet our goals."
